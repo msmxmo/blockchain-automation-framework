298df0ab07702ac989f795cdb720bff41d2ee29b
msmxmo <mary.shalom.l.maximo@accenture.com>
2021-01-14 21:57:49 +0800

    [ci skip] Pushing deployment file for storageclass
    Signed-off-by: msmxmo <mary.shalom.l.maximo@accenture.com>

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/blockchain-automation-framework/build/config
+KUBECONFIG=/home/user/workspace/blockchain-automation-framework/build/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vvv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vvv /home/user/workspace/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/user/workspace/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/user/workspace/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
